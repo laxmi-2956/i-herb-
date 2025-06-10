ee371db87c242b5bce30c0c960f5cf67046d0866
karan wagh <167848916+karanwagh17@users.noreply.github.com>
2025-06-10 17:38:10 +0530

    Update Category.jsx

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -4,7 +4,7 @@ import "../css/category.css";
 import supplementsIcon from "../assets/supplements.png";
 import sportsIcon from "../assets/Sport.png";
 import bathIcon from "../assets/Bath.png";
-import beautyIcon from "../assets/beauty.png";
+import beautyIcon from "../assets/Beauty.png";
 import groceryIcon from "../assets/Grocery_v2.png";
 import homeIcon from "../assets/Health_v2.png";
 import babyIcon from "../assets/Baby_v2.png";
