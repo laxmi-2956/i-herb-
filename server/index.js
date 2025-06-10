eda22b074833d99aa70d943021dbc5a61ad9d1c3
karan wagh <167848916+karanwagh17@users.noreply.github.com>
2025-06-10 22:14:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 // ✅ CORS setup - allow frontend deployed URL or localhost for development
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://i-herb-ten.vercel.app",
   credentials: true,
 }));
 
